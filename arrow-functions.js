a73f2e34993920fe0481875e3f21ddf2e2f22d17
Ebuka MartinsEchesi <martin4best@gmail.com>
2024-10-31 14:38:02 +0000

    creating arrays

--- arrow-functions.js
+++ arrow-functions.js
@@ -1,14 +1,37 @@
 /**
  * To run this file in Gitpod, use the 
- * command node arrow-functions.js in the terminal
+ * command node the-spread-operator.js in the terminal
  */
 
-// Vanilla JavaScript Function
+// No spread operator
 
-// Arrow Function With Parameters
+let array3 = [2,4,6];
+let array4 = array3;
+array4.push(7);
+console.log("third array", array3)
 
-// Single Line Arrow Function With Parameters
 
-// Implicit Returns
+// Copying an array
+let array1 = [2,4,6];
+let array2 = [...array1];
 
-// Returning Multiple Lines
+array2.push(10);
+console.log("first array", array1);
+console.log("second array", array2);
+
+
+
+// Copying an object
+
+let obj1 = [12,34,65,59];
+let obj2 = [...obj1, 20];
+let obj3 = [...obj1, 16];
+
+console.log("first object", obj1);
+console.log("second object", obj2);
+console.log("third object", obj3);
+
+// Copying only part of an array/object
+
+let array5 = [...array1, {...array2}, ...obj1, "z"];
+console.log(array5);
